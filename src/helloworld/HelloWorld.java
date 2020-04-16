2e70dbca1a764dd9b5a0f9e15aa3194712fbe82a
wudonglei <wudonglei@DESKTOP-US593PK>
2020-04-16 19:22:19 +0800

    version 2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello,world Ëæ±ã¸ÄÒ»¸Ä");
+System.out.println("Hello,world version2");
 }
 
 }
